e91d8cd7c71d7dfbde105e7965b27ccafd82b43e
cleciana <cleciana.santana@ccc.ufcg.edu.br>
2019-07-04 04:07:13 -0300

    a

--- src/main/java/com/example/demo/rest/configuration/MyConfiguration.java
+++ src/main/java/com/example/demo/rest/configuration/MyConfiguration.java
@@ -9,16 +9,17 @@ import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class MyConfiguration implements WebMvcConfigurer {
 
 	@Bean
-public FilterRegistrationBean corsFilter() {
-	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
-	config.addAllowedMethod("*");
-	source.registerCorsConfiguration("/**", config);
-	FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-	bean.setOrder(0);
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+		config.addAllowedMethod("*");
+		config.addAllowedOrigin("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
 	return bean;
 }
 }
