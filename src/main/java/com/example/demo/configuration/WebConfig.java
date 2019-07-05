38e050b22a45b4370b2e30e633f18b7aefee98dd
cleciana <cleciana.santana@ccc.ufcg.edu.br>
2019-07-05 16:02:31 -0300

    classeVolta

--- src/main/java/com/example/demo/configuration/WebConfig.java
+++ src/main/java/com/example/demo/configuration/WebConfig.java
@@ -1,4 +1,4 @@
-/*
+
 package com.example.demo.configuration;
 
 import org.springframework.context.annotation.Configuration;
@@ -16,4 +16,3 @@ public class WebConfig implements WebMvcConfigurer {
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 	}
 }
-*/
